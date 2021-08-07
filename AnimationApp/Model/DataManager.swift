119f83c62dbbcd84d7a8975b3698218229895de7
Miller <guf302010@gmail.com>
2021-08-07 18:21:06 +0600

    all animation cases added

--- AnimationApp/Model/DataManager.swift
+++ AnimationApp/Model/DataManager.swift
@@ -10,19 +10,11 @@ import Spring
 class DataManager {
     
     static let shared = DataManager()
-    
+
+    let name: [Spring.AnimationPreset] = Spring.AnimationPreset.allCases
+    let curve: [Spring.AnimationCurve] = Spring.AnimationCurve.allCases
     let duration: [CGFloat] = [2.5, 4.0, 3.0, 3.5, 3.2]
     let force: [CGFloat] = [2.7, 1.7, 2.0, 1.0, 1.8]
-    let name: [Spring.AnimationPreset] = [.pop,
-                                          .shake,
-                                          .flash,
-                                          .fall,
-                                          .fadeInDown ]
     
-    let curve: [Spring.AnimationCurve] = [.easeIn,
-                                          .spring,
-                                          .easeInOut,
-                                          .easeInExpo,
-                                          .easeInCirc]
     private init() {}
 }
